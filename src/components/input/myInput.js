8f1b63c3eceda4b80b2d5cb9cc3885c3358cc3d5
DzmitryYesis <yesis1988@mail.ru>
2022-08-11 08:44:58 +0300

    feat: add stateless myInput component

---
+++ src/components/input/myInput.js
@@ -0,0 +1,18 @@
+import React from "react";
+import style from './myInput.module.css'
+
+class MyInput extends React.Component {
+
+    render() {
+        return (
+            <div className={style.wrapper}>
+                <label className={style.label}>
+                    {this.props.label}:
+                </label>
+                <input className={style.input} placeholder={this.props.placeholder}/>
+            </div>
+        )
+    }
+}
+
+export default MyInput
