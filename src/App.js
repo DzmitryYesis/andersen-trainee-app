cb62a96cebc31406f40dd1aa129b8e93e4828279
DzmitryYesis <yesis1988@mail.ru>
2022-08-12 17:57:04 +0300

    feat: add App.module.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import './App.css';
+import style from './App.module.css';
 import Input from "./components/input/Input";
 import Textarea from "./components/textarea/Textarea";
 import Button from "./components/button/Button";
 
 class App extends React.Component {
     render() {
-        return <div className='App'>
-            <h1 className='HText'>Questionnaire creation</h1>
+        return <div className={style.app}>
+            <h1 className={style.hText}>Questionnaire creation</h1>
             <Input label={'Name'} placeholder={'Type your name'} type={'text'}/>
             <Input label={'Surname'} placeholder={'Type your surname'} type={'text'}/>
             <Input label={'Birthday'} placeholder={'Select'} type={'date'}/>
@@ -18,7 +18,7 @@ class App extends React.Component {
             <Textarea label={'Skills'} placeholder={'Tell us about your skills'}/>
             <Textarea label={'Previous project'}
                       placeholder={'Tell us about your previous project'}/>
-            <div className='ButtonBlock'>
+            <div className={style.buttonBlock}>
                 <Button name={'Save'}/>
                 <Button name={'Cancel'}/>
             </div>
