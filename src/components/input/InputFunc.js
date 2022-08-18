170738588c4946e512c6b94ef766047c9614b727
DzmitryYesis <yesis1988@mail.ru>
2022-08-18 09:48:12 +0300

    feat: create InputFunc component

---
+++ src/components/input/InputFunc.js
@@ -0,0 +1,15 @@
+import style from "src/components/input/Input.module.css";
+import React from "react";
+
+export const InputFunc = ({label, placeholder, type, name, onChange, value}) => {
+    return (
+        <div className={style.wrapper}>
+            <label className={style.label}>
+                {label}:
+            </label>
+            <input className={style.input} type={type}
+                   placeholder={placeholder} name={name}
+                   onChange={onChange} value={value}/>
+        </div>
+    )
+}
